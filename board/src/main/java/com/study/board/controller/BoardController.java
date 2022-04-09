defb9938e0cc2d3246d1a8c55adf05153c1ea97d
omankwon <kwon.oman@gmail.com>
2022-04-09 22:14:55 +0900

    upload git hub

---
+++ board/src/main/java/com/study/board/controller/BoardController.java
@@ -0,0 +1,36 @@
+package com.study.board.controller;
+
+import com.study.board.entity.Board;
+import com.study.board.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class BoardController {
+
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write")
+    public String boardWriteForm(){
+        return "boardwrite";
+    }
+
+    @PostMapping("/board/writepro")
+    public String boardWritePro(Board board) {
+        System.out.println("제목 :" + board.getTitle());
+
+        boardService.write(board);
+        return "test";
+    }
+
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+        model.addAttribute("list",boardService.boardList());
+
+        return "boardlist";
+    }
+}
